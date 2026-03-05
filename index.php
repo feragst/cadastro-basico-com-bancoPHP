821351904a87502d4bdc34aaf528c81c5fa8a41b
feragst <feragostini88@gmail.com>
2026-03-05 19:44:36 -0300

    index criado

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Menu Principal</title>
+    <link rel="stylesheet" href="styles.css">
+    
+</head>
+
+<body class="home">
+<div class="container">
+    <h1>Teste</h1>
+    <div class="button-group">
+        <a href="views/cadastraCliente.html" class="btn">Cadastro de Clientes</a>
+        <a href="views/cadastraVendas.html" class="btn">Cadastro de Vendas</a>
+       
+    </div>
+  
+</div>
+
+
+
+
+</body>
+
+</html>
